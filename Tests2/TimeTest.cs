c2d3a34fd7a5c69233c694875717d351d9942adf
quasar571 <snigur97@yandex.ua>
2016-03-02 01:03:31 +0200

    Edited

--- Tests2/TimeTest.cs
+++ Tests2/TimeTest.cs
@@ -14,6 +14,7 @@ using System.Web;
 using System.Web.Http;
 using System.Web.Http.Routing;
 using System.Net.Http.Formatting;
+using System.Runtime.InteropServices;
 using System.Security.Policy;
 using System.Threading;
 using TestStack.BDDfy;
@@ -33,48 +34,53 @@ namespace Tests2
 
             _server = new HttpServer(conf.Config);
         }
+        
 
         [Fact]
         public void ShouldGetCorrectTime()
         {
             var client = new HttpClient(_server);
-
-            using (var wc = new WebClient())
-            {
-                
                 //System.Diagnostics.Debugger.Launch();
 
                 var request = createRequest("", "application/json", HttpMethod.Get);
 
                 using (HttpResponseMessage response = client.SendAsync(request, new CancellationTokenSource().Token).Result)
                 {
-                    WindowsService1.Result res = JsonConvert.DeserializeObject<WindowsService1.Result>(response.Content.ReadAsStringAsync().Result);
 
-
-                    this.Given(s => s.GivenTheTimeFromServer(res.Time))
-                        .When(s => s.WhenRealTimeIs(DateTime.Now))
-                        .Then(s => s.ServerGiveActualTime())
+                    this.Given(() =>
+                        {
+                            WindowsService1.Result res = JsonConvert.DeserializeObject<WindowsService1.Result>(response.Content.ReadAsStringAsync().Result);
+                            ServTime = DateTime.Parse(res.Time);
+                        
+                        }, "Given The Time From Server " + ServTime.ToString())
+                        .When(() =>
+                        {
+                            ActualTime = DateTime.Now;
+                        }, "When Real Time Is " + ActualTime.ToString())
+                        .Then(() =>
+                        {
+                            double servUnixTime = ServTime.Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
+                            double actualUnixTime = ActualTime.Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
+                            Assert.InRange(servUnixTime, actualUnixTime - 15, actualUnixTime + 15);
+                        }, "Then Server Give Actual Time ")
                         .BDDfy();
                 }
-            }
         }
-
+        /*
         private void GivenTheTimeFromServer(string time)
         {
-            ServTime = DateTime.Parse(time);
+            
         }
 
         private void WhenRealTimeIs(DateTime acTime)
         {
-            ActualTime = acTime;
+            
         }
 
         private void ServerGiveActualTime()
         {
-            double servUnixTime = ServTime.Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
-            double actualUnixTime = ActualTime.Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
-            Assert.InRange(servUnixTime, actualUnixTime - 15, actualUnixTime + 15);
-        }
+            
+        }*/
 
         private HttpRequestMessage createRequest(string url, string mthv, HttpMethod method)
         {
