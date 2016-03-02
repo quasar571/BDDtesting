0f107f2a56d4d7117107d8ff5894c72c4c770d7e
quasar571 <snigur97@yandex.ua>
2016-03-02 22:34:46 +0200

    Edited test

--- Tests2/TimeTest.cs
+++ Tests2/TimeTest.cs
@@ -1,104 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using Xunit;
-using Newtonsoft.Json;
-using WindowsService1;
-using System.Web;
-using System.Web.Http;
-using System.Web.Http.Routing;
-using System.Net.Http.Formatting;
-using System.Runtime.InteropServices;
-using System.Security.Policy;
-using System.Threading;
+﻿using System.Net.Http;
 using TestStack.BDDfy;
+using Xunit;
 
 namespace Tests2
 {
-    public class TimeTest
+    public class TimeTest : TimeTestController
     {
-        private readonly HttpServer _server;
-        private string _url = "http://www.fortest.test/";
-        private DateTime ServTime;
-        private DateTime ActualTime;
-
-        public TimeTest()
-        {
-            Configuration conf = new Configuration(_url);
-
-            _server = new HttpServer(conf.Config);
-        }
-        
-
         [Fact]
         public void ShouldGetCorrectTime()
         {
-            var client = new HttpClient(_server);
-                //System.Diagnostics.Debugger.Launch();
-
-                var request = createRequest("", "application/json", HttpMethod.Get);
-
-                using (HttpResponseMessage response = client.SendAsync(request, new CancellationTokenSource().Token).Result)
-                {
-
-                    this.Given(() =>
-                        {
-                            WindowsService1.Result res = JsonConvert.DeserializeObject<WindowsService1.Result>(response.Content.ReadAsStringAsync().Result);
-                            ServTime = DateTime.Parse(res.Time);
-                        
-                        }, "Given The Time From Server " + ServTime.ToString())
-                        .When(() =>
-                        {
-                            ActualTime = DateTime.Now;
-                        }, "When Real Time Is " + ActualTime.ToString())
-                        .Then(() =>
-                        {
-                            double servUnixTime = ServTime.Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
-                            double actualUnixTime = ActualTime.Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
-                            Assert.InRange(servUnixTime, actualUnixTime - 15, actualUnixTime + 15);
-                        }, "Then Server Give Actual Time ")
-                        .BDDfy();
-                }
-        }
-        /*
-        private void GivenTheTimeFromServer(string time)
-        {
-            
-        }
-
-        private void WhenRealTimeIs(DateTime acTime)
-        {
-            
-        }
-
-        private void ServerGiveActualTime()
-        {
-            
-        }*/
-
-        private HttpRequestMessage createRequest(string url, string mthv, HttpMethod method)
-        {
-            var request = new HttpRequestMessage();
-
-            request.RequestUri = new Uri(_url + url);
-            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue(mthv));
-            request.Method = method;
-
-            return request;
-        }
-
-        private HttpRequestMessage createRequest<T>(string url, string mthv, HttpMethod method, T content, MediaTypeFormatter formatter) where T : class
-        {
-            HttpRequestMessage request = createRequest(url, mthv, method);
-            request.Content = new ObjectContent<T>(content, formatter);
-
-            return request;
+            this.Given(x => this.GivenTimeControllerTestContext())
+                .And(x => this.GivenRequest(HttpMethod.Get, ""))
+                .When(x => this.WhenPerformRequest())
+                .Then(x => this.ThenShouldReturnSuccessStatusCode())
+                .And(x => this.ThenPersistResult())
+                .And(x => this.ParseJsonResponse())
+                .And(x => this.ExtractServerTime())
+                .And(x => this.GetActualTime())
+                .Then(x => this.ThenCompareResults())
+                .And(x => this.DifferenceIsLessThen15())
+                .BDDfy();
         }
     }
 }
---
+++ Tests2/TimeTestController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using WindowsService1;
+using System.Web.Http;
+using System.Net.Http.Formatting;
+using System.Threading;
+using System.Web;
+using Xunit;
+
+namespace Tests2
+{
+    public class TimeTestController
+    {
+        //System.Diagnostics.Debugger.Launch();
+        protected HttpServer Server;
+        protected string Url = "http://www.fortest.test/";
+        protected HttpResponseMessage Response;
+        protected HttpRequestMessage Request;
+        protected DateTime ServTime;
+        protected DateTime ActualTime;
+        protected WindowsService1.Result Result;
+        protected double difference;
+        protected string Content;
+
+        protected void GivenTimeControllerTestContext()
+        {
+            Configuration conf = new Configuration(Url);
+
+            Server = new HttpServer(conf.Config);
+        }
+
+        public void GivenRequest(HttpMethod method, string relativeUri)
+        {
+            this.Request = this.CreateHttpRequestMessage(method, relativeUri);
+        }
+
+        public void WhenPerformRequest()
+        {
+            var client = new HttpClient(Server);
+            client.Timeout = TimeSpan.FromMilliseconds((double)int.MaxValue);
+            this.Response = client.SendAsync(this.Request).Result;
+        }
+
+        public void ThenShouldReturnSuccessStatusCode()
+        {
+            this.Response.EnsureSuccessStatusCode();
+        }
+
+        public void ThenPersistResult()
+        {
+            Content = this.Response.Content.ReadAsStringAsync().Result;
+        }
+
+        protected void ParseJsonResponse()
+        {
+            this.Result = JsonConvert.DeserializeObject<WindowsService1.Result>(Content);
+        }
+
+        protected void ExtractServerTime()
+        {
+            this.ServTime = DateTime.Parse(this.Result.Time);
+        }
+
+        protected void GetActualTime()
+        {
+            this.ActualTime = DateTime.Now;
+        }
+
+        protected void ThenCompareResults()
+        {
+            difference = ActualTime.Subtract(ServTime).TotalSeconds;
+        }
+
+        protected void DifferenceIsLessThen15()
+        {
+            Assert.InRange(difference, -15, 15);
+        }
+
+        private HttpRequestMessage CreateHttpRequestMessage(HttpMethod method, string relativeUri)
+        {
+            var requestUrl = new Uri(Url + relativeUri);
+
+            HttpContext.Current = new HttpContext(
+                new HttpRequest(string.Empty, requestUrl.ToString(), string.Empty),
+                new HttpResponse(new StringWriter()));
+
+            var request = new HttpRequestMessage { RequestUri = requestUrl };
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            request.Method = method;
+
+            return request;
+        }
+    }
+}
